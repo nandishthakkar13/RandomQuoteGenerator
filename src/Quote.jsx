3c535de419672e30b39d4e23beed52909ee9577a
Nandish Thakkar <nandishthakkar1347@gmail.com>
2020-11-01 00:25:40 -0400

    Second Commit
    configured and connected with the api, can fetch random quotes from the api.
    
    Added the function handle change, it calls the api and displays random quotes and respective author.

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -1,10 +1,57 @@
 import React, { Component } from "react";
 import "./Quote.css";
 
-//const apiUrl = "http://api.quotable.io/random";
+const apiUrl = "http://api.quotable.io/random";
 
 class Quote extends Component {
- 
+  constructor() {
+    super();
+
+    this.state = {
+      quoteDetails:{
+        quote: '',
+        quoteAuthor: ''
+      }
+    };
+
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  async componentDidMount() {
+    
+
+    const resp = await fetch(apiUrl);
+    const quoteResp = await resp.json();
+
+    this.setState(prevState =>({
+      quoteDetails:{
+        ...prevState.quoteDetails,
+        quote:quoteResp.content,
+        quoteAuthor: quoteResp.author
+      }
+
+    }));
+  }
+
+
+ async handleChange(){
+
+
+  const resp = await fetch(apiUrl);
+  const quoteResp = await resp.json();
+
+  this.setState(prevState =>({
+    quoteDetails:{
+      ...prevState.quoteDetails,
+      quote:quoteResp.content,
+      quoteAuthor: quoteResp.author
+    }
+
+  }));
+
+}
+
   render() {
     return (
       <div className="quote-container" id="quote-container">
@@ -12,19 +59,19 @@ class Quote extends Component {
           <i className="fas fa-quote-left quote-mark"></i>
           &nbsp;
           <span id="quote">
-            What you are is what you have been. What you'll be is what you do now.
+           {this.state.quoteDetails.quote}
           </span>
           &nbsp;
           <i className="fas fa-quote-right quote-mark"></i>
         </div>
         <div className="quote-author">
-          <span id="author">&#8212;Buddha</span>
+          <span id="author">&#8212;{this.state.quoteDetails.quoteAuthor}</span>
         </div>
         <div className="button-container">
           <button className="twitter-button" id="twitter" title="Tweet This!">
             <i className="fab fa-twitter"></i>
           </button>
-          <button className="new-quote" title="Get a new Title!">
+          <button className="new-quote" title="Get a new Title!" onClick={this.handleChange}>
             New Quote
           </button>
         </div>
